7fe7d4c3834646d72777befeb371fa458e7a975f
leekasong <leekasong@naver.com>
2017-08-12 15:46:15 +0900

    Update server.js

--- tips_project/server.js
+++ tips_project/server.js
@@ -1,16 +1,5 @@
 var express = require('express')
 var bodyParser = require('body-parser');
-/*var FusionCharts = require("fusioncharts")
-var chart = new FusionCharts ({
-	   "type": "column2d",
-	   "width": "500",
-	   "height": "300",
-	   "dataFormat": "json",
-	   "dataSource": {
-		    chart:{},
-		    data: [{value: 500}, {value: 600}, {value: 700}]
-	 	}
-	}).render("chartContainer");*/
 
 var app = express()
 var mysql = require('mysql')
@@ -28,17 +17,6 @@ app.get('/wether', (req, res)=>{
     res.send(data)
   })
 })
-
-/*app.post('/wether', (req, res)=>{
-  var name = req.body.name
-  var modelnumber = req.body.modelnumber
-  var series = req.body.series
-  client.query('INSERT INTO products (name, modelnumber, series) VALUES(?,?,?)', [
-    name, modelnumber, series
-  ], (error, data)=>{
-    res.send(data)
-  })
-})*/
 
 app.listen(25001, function(){
   console.log('server running at http://localhost:25001')
